b45d97780959f9a2e998fa5daf66daeeb20ce07b
simoncmh2005 <112465978+simoncmh2005@users.noreply.github.com>
2024-04-25 23:39:01 +0800

    Create signupone.java

---
+++ signupone.java
@@ -0,0 +1,23 @@
+signupone.java 
+
+1. Error message / exception JOptionpane
+
+2.Lastname and first name 
+-cannot contain numbers or symbols
+
+3.date of birth 
+--cannot earlier than today's date. Only date format.
+
+4.Gender 
+--cannot be blank
+
+5.phone number 
++852/+86 Other
+
+6.email should 
+contain @
+
+7.marital status
+cannot be blank
+
+
